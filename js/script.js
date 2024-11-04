a204fd5072d3beee2c9b70c416787e1393dcef88
David Jumeau <djumeau@gmail.com>
2024-11-04 17:21:10 -0500

    Removed items from html file and retrieved items from url

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log("Hello there.");
+const apiURL = 'https://jsonplaceholder.typicode.com/todos';
+
+const getToDos = () => {
+    fetch(apiURL + "?_limit=5")
+        .then(res => res.json())
+        .then( (data) => {
+            data.forEach((item) => {
+                const div = document.createElement('div');
+                div.appendChild(document.createTextNode(item.title));
+                document.getElementById('todo-list').appendChild(div);
+            });
+        });
+}
+
+getToDos();
