fca51513dd996b2892c4209c569693a9c2a40db2
David Jumeau <djumeau@gmail.com>
2024-11-04 17:38:45 -0500

    refactored initialization

--- js/script.js
+++ js/script.js
@@ -3,13 +3,26 @@ const apiURL = 'https://jsonplaceholder.typicode.com/todos';
 const getToDos = () => {
     fetch(apiURL + "?_limit=5")
         .then(res => res.json())
-        .then( (data) => {
-            data.forEach((item) => {
-                const div = document.createElement('div');
-                div.appendChild(document.createTextNode(item.title));
-                document.getElementById('todo-list').appendChild(div);
-            });
+        .then( data => {
+            data.forEach( item => addToDOM(item) );
         });
 }
 
-getToDos();
+const addToDOM = (item) => {
+    const div = document.createElement('div');
+    div.appendChild(document.createTextNode(item.title));
+
+    div.setAttribute('data-id', item.id);
+
+    if (item.completed) {
+        div.classList.add('done');
+    }
+
+    document.getElementById('todo-list').appendChild(div);
+}
+
+const init = () => {
+    document.addEventListener('DOMContentLoaded', getToDos);
+}
+
+init();
