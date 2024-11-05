566154269166e2f95dc95e2faeefdf8f9ef86b25
David Jumeau <djumeau@gmail.com>
2024-11-04 22:34:17 -0500

    Toggle

--- js/script.js
+++ js/script.js
@@ -10,6 +10,9 @@ const getToDos = () => {
 
 const addToDOM = (item) => {
     const div = document.createElement('div');
+
+    div.classList.add('todo');
+
     div.appendChild(document.createTextNode(item.title));
 
     div.setAttribute('data-id', item.id);
@@ -28,14 +31,47 @@ const createToDo = (e) => {
         'completed': false,
         'title': e.target.firstElementChild.value
     }
-    
-    console.log(newToDo);
+
+    fetch(apiURL, {
+        method: 'POST',
+        body: JSON.stringify(newToDo),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(res => res.json())
+        .then(data => addToDOM(data));
 }
+
+const toggleCompleted = (e) => {
+
+    e.preventDefault();
+
+    if (e.target.classList.contains('todo')) {
+        e.target.classList.toggle('done');
+        updateToDo(e.target.dataset.id, e.target.classList.contains('done'));
+    }
+    
+}
+
+const updateToDo = (id, completed) => {
+    fetch(`${apiURL}/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ completed }),
+        headers: {
+            'Content-Type' : 'application/json'
+        }
+    })
+        .then(res => res.json())
+        .then((data) => console.log(data));
+};
 
 const init = () => {
     document.addEventListener('DOMContentLoaded', getToDos);
 
     document.querySelector('#todo-form').addEventListener('submit', createToDo);
+
+    document.querySelector('#todo-list').addEventListener('click', toggleCompleted);
 }
 
 init();
